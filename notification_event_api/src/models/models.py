15b8f41ea5f3d4d539fddcac55e0bde6285745ea
arthur hodosh <hodosh87@gmail.com>
2022-09-17 22:47:53 +0300

    add models

---
+++ notification_event_api/src/models/models.py
@@ -0,0 +1,20 @@
+from typing import Any
+
+import orjson
+from pydantic import BaseModel
+
+
+def orjson_dumps(v: Any, *, default: Any) -> str:
+    return orjson.dumps(v, default=default).decode()
+
+
+class ORJSONModel(BaseModel):
+    class Config:
+        json_loads = orjson.loads
+        json_dumps = orjson_dumps
+
+
+class EnrichedNotification(ORJSONModel):
+    _id: str
+    header: str
+    body: str
